6c21a39693f45e43807d2aa9acf8bc443e23e030
Yuliia-Kyivska <kiev.lia@gmail.com>
2022-10-30 21:45:50 +0200

    2-js

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+let pictures = document.querySelector(".gallery");
+let picturesMarkup = createPicturesMarkup(galleryItems);
+pictures.insertAdjacentHTML("beforeend", picturesMarkup);
 
-console.log(galleryItems);
+
+function createPicturesMarkup(galleryItems){
+  return galleryItems.map(({ preview, original, description }) => {
+      return `
+      <a
+        class="gallery__link"
+        href = '${original}'
+      >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="Image ${description}"
+          title="beautiful picture"
+        />
+      </a>
+    `;
+    })
+    .join("");
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionDelay: 250
+});
+
+
+
+
+
+
