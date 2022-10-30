903653c0ec0e6d2c13b4390b56ebaccf66d50c99
Yuliia-Kyivska <kiev.lia@gmail.com>
2022-10-30 17:41:28 +0200

    picture gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+let pictures = document.querySelector(".gallery");
+let picturesMarkup = createPicturesMarkup(galleryItems);
+pictures.insertAdjacentHTML("beforeend", picturesMarkup);
+pictures.addEventListener('click', onClickPicture);
+
+
+function createPicturesMarkup(galleryItems){
+  return galleryItems.map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+      <a
+        class="gallery__link"
+        href = '${original}'
+      >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+   </div>
+    `;
+    })
+    .join("");
+}
+
+function onClickPicture(evt) {
+  evt.preventDefault();
+  if (!evt.target.classList.contains('gallery__image')) {
+    return;
+  }
+}
+
+
 
 console.log(galleryItems);
+
+
+
+
+
+
